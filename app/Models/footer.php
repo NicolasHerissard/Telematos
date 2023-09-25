b57f40b8f0ea4c96212c0e994bd13744299cf952
lakpoo <kiliandominik93@gmail.com>
2023-09-25 19:57:00 +0200

    Correction bordel dominik

--- app/Models/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-<form id="copyright" class="button" style="width: 120px;">© Copyright 2023</form>
-<script> document.getElementById("copyright").addEventListener("click", function(event) { 
-    event.preventDefault(); alert("Copyright TKT"); }); </script>
-</footer>
-</body>
-</html>
