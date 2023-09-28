c64af5374e5e03360dcbfdb778da3735eb4d0870
lakpoo <kiliandominik93@gmail.com>
2023-09-28 09:47:08 +0200

    message d'erreur avec css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -43,7 +43,7 @@
                 <div class="error">
                     {{ Session::get('error') }}
                 </div>
-                @endif
+            @endif
         </div>
     </div>
 <!--
