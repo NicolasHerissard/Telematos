fbad75d513c34dc50f563bf4ea8382970beb64d7
lakpoo <kiliandominik93@gmail.com>
2023-11-06 15:57:49 +0100

    correction bug git

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -6,15 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="/css/reset.css" rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
-<<<<<<< HEAD
     <link rel="icon" type="image/png" href="/../logo_navecran64x64.png">
-=======
-<<<<<<< HEAD
-    <link rel="icon" type="image/png" href="/../logo_navecran64x64.png">
-=======
-    <link rel="icon" type="image/png" href="logo_navecran64x64.png">
->>>>>>> 9bccbba1f9257a037c100f53018b2acf3f252c5c
->>>>>>> a1d1f3e62d4a0c4cf2cb7d24c403f677afc8eaba
     <title>Création utilisateurs</title>
 </head>
 <body>
@@ -24,10 +16,6 @@
     <div class="body">
 
         @include('component/sidebar')
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> a1d1f3e62d4a0c4cf2cb7d24c403f677afc8eaba
         <div class="create-all">
             <div class="form-inscr-conn">
                 <form class="form-crea-compte" action="{{ route('admin.createUsers') }}" method="post">
@@ -44,36 +32,6 @@
                 </form>
             </div>
         </div>
-<<<<<<< HEAD
-=======
-=======
-
-        <form action="{{ route('admin.createUsers') }}" method="post">
-            @csrf
-                <div class="create-users">
-                    <div class="name-users">
-                        <label for="name">Nom</label>
-                        <input type="text" name="name" placeholder="Nom">
-                    </div>
-                    <div class="email-users">
-                        <label for="email">Email</label>
-                        <input type="email" name="email" placeholder="email">
-                    </div>
-                    <div class="password-users">
-                        <label for="password">Mot de passe</label>
-                        <input type="password" name="password" placeholder="password">
-                    </div>
-                    <div class="role-users">
-                        <label for="role">role</label>
-                        <input type="text" name="isadmin" placeholder="role">
-                    </div>
-                    <div class="btn-submit">
-                        <button type="submit">Enregistrer</button>
-                    </div>
-                </div>
-        </form>
->>>>>>> 9bccbba1f9257a037c100f53018b2acf3f252c5c
->>>>>>> a1d1f3e62d4a0c4cf2cb7d24c403f677afc8eaba
     </div>
 </body>
 </html>
--- resources/views/users/showProductUser.blade.php
+++ resources/views/users/showProductUser.blade.php
@@ -6,15 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="/css/reset.css" rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
-<<<<<<< HEAD
     <link rel="icon" type="image/png" href="/../logo_navecran64x64.png">
-=======
-<<<<<<< HEAD
-    <link rel="icon" type="image/png" href="/../logo_navecran64x64.png">
-=======
-    <link rel="icon" type="image/png" href="logo_navecran64x64.png">
->>>>>>> 9bccbba1f9257a037c100f53018b2acf3f252c5c
->>>>>>> a1d1f3e62d4a0c4cf2cb7d24c403f677afc8eaba
     <title>Mon matériel</title>
 </head>
 <body>
